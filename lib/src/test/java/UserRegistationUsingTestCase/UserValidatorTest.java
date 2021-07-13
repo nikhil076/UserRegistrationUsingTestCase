cd30fff48522f3130b3bd8f89ac7cc7d4e759456
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-13 17:16:48 +0530

    Added the test case to check the first name

--- lib/src/test/java/UserRegistationUsingTestCase/UserValidatorTest.java
+++ lib/src/test/java/UserRegistationUsingTestCase/UserValidatorTest.java
@@ -1,6 +1,32 @@
 package UserRegistationUsingTestCase;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class UserValidatorTest {
 
-
+	//Test case for First Name
+	@Test
+	public void givenFirstName_WhenitsSpecailChar_ShouldReturnFalse()
+	{
+		UserRegistationValidator registationValidator = new UserRegistationValidator();
+		boolean result = registationValidator.firstNameRegex("Nikhil");
+		Assert.assertEquals(true, result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenItIsLessThan3Char_ShouldReturnFalse() 
+	{
+		UserRegistationValidator registationValidator = new UserRegistationValidator();
+		boolean result = registationValidator.firstNameRegex("Ni");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenFirstName_WhenItIsValid_ShouldReturnTrue()
+	{
+		UserRegistationValidator registationValidator = new UserRegistationValidator();
+		boolean result = registationValidator.firstNameRegex("NikH!l");
+		Assert.assertEquals(false, result);
+	}
 }
