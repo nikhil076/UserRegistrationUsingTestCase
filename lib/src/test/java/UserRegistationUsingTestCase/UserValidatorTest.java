93f0992557794681599dcac284c99591f7160fb9
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-13 17:35:47 +0530

    Added the test case to check the Last name

--- lib/src/test/java/UserRegistationUsingTestCase/UserValidatorTest.java
+++ lib/src/test/java/UserRegistationUsingTestCase/UserValidatorTest.java
@@ -7,7 +7,7 @@ public class UserValidatorTest {
 
 	//Test case for First Name
 	@Test
-	public void givenFirstName_WhenitsSpecailChar_ShouldReturnFalse()
+	public void givenFirstName_WhenItIsValid_ShouldReturnTrue()
 	{
 		UserRegistationValidator registationValidator = new UserRegistationValidator();
 		boolean result = registationValidator.firstNameRegex("Nikhil");
@@ -23,10 +23,36 @@ public class UserValidatorTest {
 	}
 	
 	@Test
-	public void givenFirstName_WhenItIsValid_ShouldReturnTrue()
+	public void givenFirstName_WhenitsSpecailChar_ShouldReturnFalse()
 	{
 		UserRegistationValidator registationValidator = new UserRegistationValidator();
 		boolean result = registationValidator.firstNameRegex("NikH!l");
 		Assert.assertEquals(false, result);
 	}
+	
+	//Test case for Last Name
+	@Test
+	public void givenLastName_WhenItsValid_ShouldReturnTrue()
+	{
+		UserRegistationValidator registationValidator = new UserRegistationValidator();
+		boolean result = registationValidator.lastNameRegex("Arvind");
+		Assert.assertEquals(true, result);
+	}
+	
+	@Test
+	public void givenLastName_WhenItIsLessThan3Char_ShouldReturnFalse() 
+	{
+		UserRegistationValidator registationValidator = new UserRegistationValidator();
+		boolean result = registationValidator.lastNameRegex("Av");
+		Assert.assertEquals(false, result);
+	}
+	
+	@Test
+	public void givenLastName_WhenitsSpecailChar_ShouldReturnFalse()
+	{
+		UserRegistationValidator registationValidator = new UserRegistationValidator();
+		boolean result = registationValidator.lastNameRegex("Arv!nd");
+		Assert.assertEquals(false, result);
+	}
 }
+
