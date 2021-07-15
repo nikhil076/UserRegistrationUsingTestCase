25f69b367eb6c11b0e08e3259de40091cff263f2
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-15 23:02:03 +0530

    Added functional interface and created lambda function to validate user details

--- lib/src/main/java/UserRegistationUsingTestCase/UserRegistationValidator.java
+++ lib/src/main/java/UserRegistationUsingTestCase/UserRegistationValidator.java
@@ -5,82 +5,16 @@ import java.util.regex.Pattern;
 
 import UserRegistationUsingTestCase.UserRegistrationException.ExceptionType;
 
+@FunctionalInterface
+interface Validate {
+	Boolean validator(String name , String regex);
+}
+
 public class UserRegistationValidator 
 {
-
-	public boolean firstNameRegex(String firstName) throws UserRegistrationException 
-	{
-		try 
-		{
-			String firstNameRegex = "^[A-Z][a-z]{2,}";
-			Pattern pattern = Pattern.compile(firstNameRegex);
-			Matcher matched = pattern.matcher(firstName);
-			return matched.matches();
-		}
-		catch (Exception e) 
-		{
-			throw new UserRegistrationException(ExceptionType.ENTERED_NULL , "Please Enter a valid First Name");
-		}
-	}
-
-	public boolean lastNameRegex(String lastName) throws UserRegistrationException
-	{
-		try
-		{
-			String firstNameRegex = "^[A-Z][a-z]{2,}";
-			Pattern pattern = Pattern.compile(firstNameRegex);
-			Matcher matched = pattern.matcher(lastName);
-			return matched.matches();
-		}
-		catch (Exception e) 
-		{
-			throw new UserRegistrationException(ExceptionType.ENTERED_NULL ,"Please Enter a valid Last Name");
-		}
-	}
-
-	public boolean emailRegex(String email) throws UserRegistrationException 
-	{
-		try
-		{
-			String emailRegex = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-			Pattern pattern = Pattern.compile(emailRegex);
-			Matcher matched = pattern.matcher(email);
-			return matched.matches();
-		}
-		catch (Exception e) 
-		{
-			throw new UserRegistrationException(ExceptionType.ENTERED_NULL ,"Please Enter a valid email");
-		}
-	}
-
-	public boolean phoneNumberRegex(String phoneNumber) throws UserRegistrationException 
-	{
-		try
-		{
-			String phoneNumberRegex = "^[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$";
-			Pattern pattern = Pattern.compile(phoneNumberRegex);
-			Matcher matched = pattern.matcher(phoneNumber);
-			return matched.matches();
-		}
-		catch (Exception e) 
-		{
-			throw new UserRegistrationException(ExceptionType.ENTERED_NULL ,"Please Enter a valid phone number");
-		}
-	}
-
-	public boolean passwordRegex(String password) throws UserRegistrationException 
-	{
-		try
-		{
-			String passwordRegex = "(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}";
-			Pattern pattern = Pattern.compile(passwordRegex);
-			Matcher matched = pattern.matcher(password);
-			return matched.matches();
-		}
-		catch (Exception e) 
-		{
-			throw new UserRegistrationException(ExceptionType.ENTERED_NULL ,"Please Enter a valid password");
-		}
-	}
-
+	Validate validatorDetails = (firstName,regex) -> {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matched = pattern.matcher(firstName); 
+		return matched.matches();
+	};
 }
--- lib/src/main/java/UserRegistationUsingTestCase/UserRegistrationMain.java
+++ lib/src/main/java/UserRegistationUsingTestCase/UserRegistrationMain.java
@@ -4,11 +4,10 @@ public class UserRegistrationMain {
 
 	public static void main(String[] args) throws UserRegistrationException {
 		UserRegistationValidator registationValidator = new UserRegistationValidator();
-		System.out.println("First Name :"+registationValidator.firstNameRegex("Nikhil"));
-		System.out.println("Last Name :"+registationValidator.lastNameRegex("Arvind"));
-		System.out.println("Phone Number :"+registationValidator.phoneNumberRegex("91 7066889759"));
-		System.out.println("email :"+registationValidator.emailRegex("nikhil.arvind@gmail.com"));
-		System.out.println("password :"+registationValidator.passwordRegex("Nikhil@123"));
+		System.out.println("First name is :"+registationValidator.validatorDetails.validator("Nikhil", "^[A-Z][a-z]{2,}"));
+		System.out.println("Last name is :"+registationValidator.validatorDetails.validator("Arvind", "^[A-Z][a-z]{2,}"));
+		System.out.println("Phone number is :"+registationValidator.validatorDetails.validator("91 7066889759", "^[0-9]{1,3}[' '][0-9]{10}$"));
+		System.out.println("Email is :"+registationValidator.validatorDetails.validator("nikhil.arvind@gmail.com", "^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$"));
+		System.out.println("password is :"+registationValidator.validatorDetails.validator("Nikhil@123", "(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}"));
 	}
-
 }
